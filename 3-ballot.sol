b05fc8ec0400177efca3824670f36281f38ae2e4
Francisco José Medrano <66084146+Linceiberico59@users.noreply.github.com>
2020-10-21 12:51:49 +0200

    Add files via upload

--- 3-ballot.sol
+++ 3-ballot.sol
@@ -10,5 +10,10 @@ contract Inbox {
     function setMessage(string newMessage) public {
         message = newMessage;
     }
-
+    function doMathic(int a, int b) {
+        a + b;
+        b - a;
+        a * b;
+        a == 0;
+    }
 }
